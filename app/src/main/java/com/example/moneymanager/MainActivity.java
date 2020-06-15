da3757d893368cb4ae78040769648560e9484ed3
Phan Nhật Vinh <phannhatvinh10a@gmail.com>
2020-06-15 13:10:55 +0700

    Added base layouts

---
+++ app/src/main/java/com/example/moneymanager/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.moneymanager;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    final Fragment fragment1 = new HomeFragment();
+    final Fragment fragment2 = new StatisticsFragment();
+    final Fragment fragment3 = new UltilitiesFragment();
+    final Fragment fragment4 = new SettingsFragment();
+    final FragmentManager fm = getSupportFragmentManager();
+    Fragment active = fragment1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        //Fragment controls
+        fm.beginTransaction().add(R.id.main_container, fragment4, "4").hide(fragment4).commit();
+        fm.beginTransaction().add(R.id.main_container, fragment3, "3").hide(fragment3).commit();
+        fm.beginTransaction().add(R.id.main_container, fragment2, "2").hide(fragment2).commit();
+        fm.beginTransaction().add(R.id.main_container,fragment1, "1").commit();
+
+        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.navigation_home:
+                    {
+                        fm.beginTransaction().hide(active).show(fragment1).commit();
+                        active = fragment1;
+                        return true;
+                    }
+                    case R.id.navigation_statistics:
+                    {
+                        fm.beginTransaction().hide(active).show(fragment2).commit();
+                        active = fragment2;
+                        return true;
+                    }
+                    case R.id.navigation_add:
+                    {
+                        return true;
+                    }
+                    case R.id.navigation_utilities:
+                    {
+                        fm.beginTransaction().hide(active).show(fragment3).commit();
+                        active = fragment3;
+                        return true;
+                    }
+                    case R.id.navigation_settings:
+                    {
+                        fm.beginTransaction().hide(active).show(fragment4).commit();
+                        active = fragment4;
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
+        navView.getMenu().getItem(2).setEnabled(false);
+        //End fragment controls
+    }
+}
