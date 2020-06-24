0bdc29a326469ac59eacac61367e4402f8a689b3
Vinh <vinhdang871@gmail.com>
2020-06-24 21:15:00 +0700

    Update change user password with re-authentication

--- app/src/main/java/com/example/moneymanager/EditUserInfoActivity.java
+++ app/src/main/java/com/example/moneymanager/EditUserInfoActivity.java
@@ -18,10 +18,17 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.UserInfo;
 import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class EditUserInfoActivity extends AppCompatActivity {
@@ -52,6 +59,14 @@ public class EditUserInfoActivity extends AppCompatActivity {
 
         fullNameET.setText(user.getDisplayName());
 
+        UserInfo userInfo = FirebaseAuth.getInstance().getCurrentUser().getProviderData().get(1);
+        if (userInfo.getProviderId().equals("google.com") || userInfo.getProviderId().equals("facebook.com")) {
+            findViewById(R.id.newPasswordTV).setVisibility(View.GONE);
+            passWordET.setVisibility(View.GONE);
+            findViewById(R.id.reNewPasswordTV).setVisibility(View.GONE);
+            rePassWordET.setVisibility(View.GONE);
+        }
+
         //Nút back
         findViewById(R.id.backButton_editUserInfo).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,7 +80,7 @@ public class EditUserInfoActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 final String fullName = fullNameET.getText().toString().trim();
-                String passWord = passWordET.getText().toString().trim();
+                final String passWord = passWordET.getText().toString().trim();
                 String rePassWord = rePassWordET.getText().toString().trim();
 
                 //Tạo dialog lỗi
@@ -93,46 +108,84 @@ public class EditUserInfoActivity extends AppCompatActivity {
                 if(!TextUtils.isEmpty(passWord)) {
                     if(passWord.compareTo(rePassWord) != 0) {
                         alertDialog.show();
-                        message.setText("Nhập lại mật khẩu không trùng khớp!");
+                        message.setText("Nhập lại mật khẩu sai!");
                         return;
                     }
 
-                    loading.setVisibility(View.VISIBLE);
-                    container.setVisibility(View.INVISIBLE);
-                    // Cập nhập password (chưa hoàn thành xong - re-authentication)
-                    user.updatePassword(passWord).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    //Tạo dialog nhập mật khẩu cũ
+                    AlertDialog.Builder opBuilder = new AlertDialog.Builder(v.getContext());
+                    opBuilder.setView(R.layout.dialog_fill_oldpassword);
+                    final AlertDialog opAlertDialog = opBuilder.create();
+                    opAlertDialog.show();
+                    opAlertDialog.getWindow().setLayout(850,700);
+                    opAlertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                    final EditText oldPasswordET = opAlertDialog.findViewById(R.id.oldPassword);
+                    opAlertDialog.findViewById(R.id.cancel_two_button_dialog).setOnClickListener(new View.OnClickListener() {
                         @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if(task.isSuccessful()) {
-                                if(user.getDisplayName().compareTo(fullName) != 0) {
-                                    UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                                            .setDisplayName(fullName)
-                                            .build();
+                        public void onClick(View v) {
+                            opAlertDialog.dismiss();
+                        }
+                    });
+                    opAlertDialog.findViewById(R.id.confirm_two_button_dialog).setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            opAlertDialog.dismiss();
 
-                                    user.updateProfile(profileUpdates)
-                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                @Override
-                                                public void onComplete(@NonNull Task<Void> task) {
-                                                    if (task.isSuccessful()) {
-                                                        new DataHelper().setUsersCollection(user.getUid(), user.getDisplayName(), user.getEmail(), user.getPhotoUrl() != null ? user.getPhotoUrl().toString() : "");
-                                                        Toast.makeText(EditUserInfoActivity.this, "Cập nhập thông tin thành công", Toast.LENGTH_SHORT).show();
+                            // Đăng nhập lại
+                            loading.setVisibility(View.VISIBLE);
+                            container.setVisibility(View.INVISIBLE);
 
+                            AuthCredential credentials = EmailAuthProvider.getCredential(user.getEmail(), oldPasswordET.getText().toString().trim());
+                            user.reauthenticate(credentials).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task1) {
+                                    if(task1.isSuccessful()) {
+
+                                        // Cập nhập password
+                                        user.updatePassword(passWord).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task2) {
+                                                if(task2.isSuccessful()) {
+                                                    if(user.getDisplayName().compareTo(fullName) != 0) {
+                                                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                                                .setDisplayName(fullName)
+                                                                .build();
+
+                                                        user.updateProfile(profileUpdates)
+                                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                    @Override
+                                                                    public void onComplete(@NonNull Task<Void> task3) {
+                                                                        if (task3.isSuccessful()) {
+                                                                            new DataHelper().setUsersCollection(user.getUid(), user.getDisplayName(), user.getEmail(), user.getPhotoUrl() != null ? user.getPhotoUrl().toString() : "");
+                                                                            Toast.makeText(EditUserInfoActivity.this, "Cập nhập thông tin thành công", Toast.LENGTH_SHORT).show();
+
+                                                                            alertDialog.dismiss();
+                                                                            setResult(Activity.RESULT_OK, new Intent());
+                                                                            finish();
+                                                                        }
+                                                                    }
+                                                                });
+                                                    } else {
+                                                        Toast.makeText(EditUserInfoActivity.this, "Cập nhập mật khẩu thành công", Toast.LENGTH_SHORT).show();
                                                         alertDialog.dismiss();
-                                                        setResult(Activity.RESULT_OK, new Intent());
                                                         finish();
                                                     }
                                                 }
-                                            });
-                                } else {
-                                    alertDialog.dismiss();
-                                    finish();
+                                                else {
+                                                    loading.setVisibility(View.GONE);
+                                                    container.setVisibility(View.VISIBLE);
+                                                    Toast.makeText(EditUserInfoActivity.this , "Đổi mật khẩu thất bại" + task2.getException(), Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
+                                    }
+                                    else {
+                                        loading.setVisibility(View.GONE);
+                                        container.setVisibility(View.VISIBLE);
+                                        Toast.makeText(EditUserInfoActivity.this , "Đổi mật khẩu thất bại" + task1.getException(), Toast.LENGTH_SHORT).show();
+                                    }
                                 }
-                            }
-                            else {
-                                loading.setVisibility(View.GONE);
-                                container.setVisibility(View.VISIBLE);
-                                Toast.makeText(EditUserInfoActivity.this , "Đổi mật khẩu thất bại" + task.getException(), Toast.LENGTH_SHORT).show();
-                            }
+                            });
                         }
                     });
 
