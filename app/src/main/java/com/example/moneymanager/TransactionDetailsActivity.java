1a4185c36c967d2fbfdc9e0f3b400dbfa2b446b7
Phan Nhật Vinh <phannhatvinh10a@gmail.com>
2020-08-02 17:00:39 +0700

    Fixed Transaction Details

--- app/src/main/java/com/example/moneymanager/TransactionDetailsActivity.java
+++ app/src/main/java/com/example/moneymanager/TransactionDetailsActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class TransactionDetailsActivity extends AppCompatActivity {
 
@@ -16,6 +21,51 @@ public class TransactionDetailsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        final Button saveOrEditButton = findViewById(R.id.editAndSave_transactionDetails);
+        final Button chooseDateTimeButton = findViewById(R.id.chooseDateTime_transactionDetails);
+        final EditText dateTime = findViewById(R.id.dateTime_transactionDetails);
+        final EditText description = findViewById(R.id.description_transactionDetails);
+        final EditText amount = findViewById(R.id.amount_transactionDetails);
+        final ImageView icon = findViewById(R.id.transactionIcon_transactionDetails);
+
+        saveOrEditButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String currentStatus = (String) saveOrEditButton.getText();
+
+                if(currentStatus.equals("Sửa"))
+                {
+
+                    LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(
+                            0,LinearLayout.LayoutParams.MATCH_PARENT,3.0f);
+
+                    icon.setOnClickListener(null);
+                    amount.setEnabled(true);
+                    dateTime.setLayoutParams(param);
+                    chooseDateTimeButton.setVisibility(View.VISIBLE);
+                    description.setEnabled(true);
+
+                    saveOrEditButton.setText("Lưu");
+
+                }
+                else
+                {
+                    LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(
+                            0,LinearLayout.LayoutParams.MATCH_PARENT,4.0f);
+
+                    icon.setOnClickListener(null); //Thêm on click cho icon, mở dialog_choose_category.xml
+                    amount.setEnabled(false);
+                    dateTime.setLayoutParams(param);
+                    chooseDateTimeButton.setVisibility(View.GONE);
+                    description.setEnabled(false);
+
+                    saveOrEditButton.setText("Sửa");
+
+                }
+
             }
         });
 
