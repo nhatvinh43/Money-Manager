e53e58b20ff8a6d7097e8e06f061699a66be1707
lethanhtruongvinh99 <lethanhtruongvinh99@gmail.com>
2020-08-05 19:49:56 +0700

    fix bugs

--- app/src/main/java/com/example/moneymanager/UltilitiesFragment.java
+++ app/src/main/java/com/example/moneymanager/UltilitiesFragment.java
@@ -1,5 +1,6 @@
 package com.example.moneymanager;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -104,7 +106,7 @@ public class UltilitiesFragment extends Fragment {
                     public void onClick(View v) {
                         Intent intent = new Intent(v.getContext(), AddMoneySourceActivity.class);
                         categoryPanel.dismiss();
-                        startActivity(intent);
+                        startActivityForResult(intent, 1);
 
                     }
                 });
@@ -120,7 +122,8 @@ public class UltilitiesFragment extends Fragment {
                         MoneySource moneySource = dataSet.get(position);
                         Intent intent = new Intent(categoryPanel.getContext(), MoneySourceDetailsActivity.class);
                         intent.putExtra("MSId", moneySource.getMoneySourceId());
-                        startActivity(intent);
+                        categoryPanel.dismiss();
+                        startActivityForResult(intent, 2);
                     }
                 });
 
@@ -223,9 +226,18 @@ public class UltilitiesFragment extends Fragment {
                 });
             }
         });
+    }
 
-
-
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 || requestCode == 2){
+            if (resultCode == 1) {
+                dataSet.clear();
+                dataSet = dataHelper.getListMoneySource(uId);
+                adapter.notifyDataSetChanged();
+            }
+        }
     }
 
     @Override
