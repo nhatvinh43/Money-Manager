6b83d4b7de38cd258fa6ba039ddc57a54342d6b1
lethanhtruongvinh99 <lethanhtruongvinh99@gmail.com>
2020-08-07 01:18:53 +0700

    demo chart

--- app/src/main/java/com/example/moneymanager/StatisticsFragment.java
+++ app/src/main/java/com/example/moneymanager/StatisticsFragment.java
@@ -9,22 +9,45 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.PagerSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
 
+import android.os.Handler;
 import android.text.format.DateUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LayoutAnimationController;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
+import com.google.firebase.auth.FirebaseAuth;
 import com.whiteelephant.monthpicker.MonthPickerDialog;
 
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
 import static android.content.Context.MODE_PRIVATE;
@@ -50,8 +73,20 @@ public class StatisticsFragment extends Fragment {
     TextView dateHome;
     TextView dateArrow;
     int mYear, mMonth, mDay;
+    private ArrayList<MoneySource> moneySouces = new ArrayList<>();
+    private ArrayList<Transaction> transactions = new ArrayList<>();
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private DataHelper dataHelper = new DataHelper();
+    private RecyclerView recyclerView;
+    private HomeMoneySourceAdapter homeMoneySourceAdapter;
+    private MoneySource selectedMoneySource = new MoneySource();
+    private LayoutAnimationController layoutAnimationController;
 
-    enum ViewMode {
+    private String[] overallTitle = {"Thu", "Chi"};
+    private AnyChartView overallChart;
+
+    ViewMode2 viewMode2 = ViewMode2.DAY;
+    enum ViewMode2 {
         DAY,
         WEEK,
         MONTH,
@@ -99,13 +134,42 @@ public class StatisticsFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        final ProgressBar loading = view.findViewById(R.id.loading_statistics);
+        final RelativeLayout rl = view.findViewById(R.id.statisticsContainer);
 
+        dataHelper.getMoneySource(new MoneySourceCallBack() {
+            @Override
+            public void onCallBack(ArrayList<MoneySource> list) {
+                getMoneySourceList(list);
+                loading.setVisibility(View.GONE);
+                rl.setVisibility(View.VISIBLE);
+                initView(view);
+            }
+
+            @Override
+            public void onCallBackFail(String message) {
+
+            }
+        }, firebaseAuth.getCurrentUser().getUid());
+    }
+
+    public void getMoneySourceList(ArrayList<MoneySource> list){
+        moneySouces.clear();
+        moneySouces.addAll(list);
+        selectedMoneySource = moneySouces.get(0);
+        transactions = selectedMoneySource.getTransactionsList();
+    }
+
+    public void initView(View view){
         // Calendar Initiation
         dayOfWeek = view.findViewById(R.id.weekDay_statistics);
         dateHome = view.findViewById(R.id.date_statistics);
         dateArrow = view.findViewById(R.id.dateArrow_statistics);
+        recyclerView = view.findViewById(R.id.moneySourceList_statistics);
+        overallChart = view.findViewById(R.id.overallChart);
+
 
         SharedPreferences prefs = ((MainActivity)getActivity()).getSharedPreferences("MyPreferences", MODE_PRIVATE);
         String myFormat = prefs.getString("currentDate", "dd/MM/yyyy");
@@ -125,43 +189,59 @@ public class StatisticsFragment extends Fragment {
 
         ArrayAdapter<String> viewByAdapter = new ArrayAdapter<String>(view.getContext(), R.layout.spinner_menu, viewByMenuItems);
         viewByMenu.setAdapter(viewByAdapter);
+
         //NOTE: file layout của dialog chọn khoảng thời gian là dialog_choose_time.xml
         viewByMenu.setSelection(0);
         viewByMenu.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+                Calendar myCalender = Calendar.getInstance();
+                mYear = myCalender.get(Calendar.YEAR);
+                mMonth = myCalender.get(Calendar.MONTH);
+                mDay = myCalender.get(Calendar.DAY_OF_MONTH);
                 String selectedItem = parent.getItemAtPosition(position).toString();
                 switch (selectedItem)
                 {
                     case "Ngày":
                     {
-                        viewMode = HomeFragment.ViewMode.DAY;
+                        dateHome.setText(sdf.format(myCalender.getTime()));
+                        dayOfWeek.setText("Hôm nay");
+                        viewMode2 = viewMode2.DAY;
+
+                        transactions.clear();
+                        transactions.addAll(modifierTransactionListByViewMode());
+                        setupDateChart();
                         break;
                     }
                     case "Tuần":
                     {
-                        viewMode = HomeFragment.ViewMode.WEEK;
+                        viewMode2 = viewMode2.WEEK;
                         break;
                     }
                     case "Tháng":
                     {
-                        viewMode = HomeFragment.ViewMode.MONTH;
+                        SimpleDateFormat sdf_month = new SimpleDateFormat("MM/yyyy");
+                        dateHome.setText(sdf_month.format(myCalender.getTime()));
+                        dayOfWeek.setText("Tháng nay");
+                        viewMode2 = viewMode2.MONTH;
                         break;
                     }
                     case "Quý":
                     {
-                        viewMode = HomeFragment.ViewMode.QUARTER;
+                        viewMode2 = viewMode2.QUARTER;
                         break;
                     }
                     case "Năm":
                     {
-                        viewMode = HomeFragment.ViewMode.YEAR;
+                        SimpleDateFormat sdf_month = new SimpleDateFormat("yyyy");
+                        dateHome.setText(sdf_month.format(myCalender.getTime()));
+                        dayOfWeek.setText("Năm nay");
+                        viewMode2 = viewMode2.YEAR;
                         break;
                     }
                     case "Chọn":
                     {
-                        viewMode = HomeFragment.ViewMode.MANUAL;
+                        viewMode2 = viewMode2.MANUAL;
 
                         androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(getContext());
                         builder.setView(R.layout.dialog_choose_time);
@@ -188,10 +268,11 @@ public class StatisticsFragment extends Fragment {
             @Override
             public void onClick(View v) {
 
-                switch(viewMode)
+                switch(viewMode2)
                 {
                     case DAY:
                     {
+
                         DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
                             @Override
                             public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
@@ -211,6 +292,11 @@ public class StatisticsFragment extends Fragment {
                                 mMonth = myCalendar.get(Calendar.MONTH);
                                 mDay = myCalendar.get(Calendar.DAY_OF_MONTH);
 
+                                transactions.clear();
+                                transactions.addAll(modifierTransactionListByViewMode());
+                                setupDateChart();
+                                System.out.println("In: " + modifierTransactionListByViewMode().size());
+
                             }
                         };
                         new DatePickerDialog(getContext(), R.style.DatePickerDialog, date, mYear, mMonth, mDay).show();
@@ -229,7 +315,20 @@ public class StatisticsFragment extends Fragment {
                                     @Override
                                     public void onDateSet(int selectedMonth, int selectedYear)
                                     {
-                                        //Code code code
+                                        SimpleDateFormat sdf_month = new SimpleDateFormat("MM/yyyy");
+                                        Calendar myCalendar = Calendar.getInstance();
+                                        myCalendar.set(Calendar.YEAR, selectedYear);
+                                        myCalendar.set(Calendar.MONTH, selectedMonth);
+                                        dateHome.setText(sdf_month.format(myCalendar.getTime()));
+
+                                        if(selectedMonth == Calendar.getInstance().get(Calendar.MONTH) && selectedYear == Calendar.getInstance().get(Calendar.YEAR)) {
+                                            dayOfWeek.setText("Tháng này");
+                                        } else {
+                                            dayOfWeek.setText("Tháng " + (selectedMonth + 1));
+                                        }
+
+                                        mYear = myCalendar.get(Calendar.YEAR);
+                                        mMonth = myCalendar.get(Calendar.MONTH);
                                     }
                                 }, today.get(Calendar.YEAR), today.get(Calendar.MONTH));
                         builder.setActivatedMonth(Calendar.JULY)
@@ -258,7 +357,18 @@ public class StatisticsFragment extends Fragment {
                                     @Override
                                     public void onDateSet(int selectedMonth, int selectedYear)
                                     {
-                                        //Bỏ qua month
+                                        SimpleDateFormat sdf_month = new SimpleDateFormat("yyyy");
+                                        Calendar myCalendar = Calendar.getInstance();
+                                        myCalendar.set(Calendar.YEAR, selectedYear);
+                                        dateHome.setText(sdf_month.format(myCalendar.getTime()));
+
+                                        if(selectedYear == Calendar.getInstance().get(Calendar.YEAR)) {
+                                            dayOfWeek.setText("Năm nay");
+                                        } else {
+                                            dayOfWeek.setText("Năm " + selectedYear);
+                                        }
+
+                                        mYear = myCalendar.get(Calendar.YEAR);
                                     }
                                 }, today.get(Calendar.YEAR), today.get(Calendar.MONTH));
                         builder.setActivatedMonth(Calendar.JULY)
@@ -290,6 +400,144 @@ public class StatisticsFragment extends Fragment {
         dateArrow.setOnClickListener(dateSelector);
         dayOfWeek.setOnClickListener(dateSelector);
         dateHome.setOnClickListener(dateSelector);
+        //init recyclerView
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        final SnapHelper snapHelper = new PagerSnapHelper();
+        snapHelper.attachToRecyclerView(recyclerView);
+
+        homeMoneySourceAdapter = new HomeMoneySourceAdapter(moneySouces, getContext());
+        recyclerView.setAdapter(homeMoneySourceAdapter);
+        recyclerView.setClipToPadding(false);
+        int activeColor = ContextCompat.getColor(view.getContext(), R.color.white);
+        int deactiveColor = ContextCompat.getColor(view.getContext(), R.color.colorPrimaryDark);
+        recyclerView.addItemDecoration(new DotsIndicatorDecoration(9, 20, 100, deactiveColor, activeColor));
+
+        //Money source Item animation
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                RecyclerView.ViewHolder viewHolder = recyclerView.findViewHolderForAdapterPosition(0);
+//                CardView cv = viewHolder.itemView.findViewById(R.id.cardContainer);
+//                cv.animate().setDuration(100).scaleX(1).scaleY(1).setInterpolator(new AccelerateInterpolator()).start();
+            }
+        }, 100);
+
+        //on Snap Helper Scroll
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (newState == RecyclerView.SCROLL_STATE_DRAGGING){
+                    return;
+                }
+                View v = snapHelper.findSnapView(linearLayoutManager);
+                int pos = linearLayoutManager.getPosition(v);
+
+                RecyclerView.ViewHolder viewHolder = recyclerView.findViewHolderForAdapterPosition(pos);
+                CardView cv = viewHolder.itemView.findViewById(R.id.cardContainer);
+
+                if(newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    cv.animate().setDuration(300).scaleX(1).scaleY(1).setInterpolator(new DecelerateInterpolator()).start();
+                } else {
+                    cv.animate().setDuration(300).scaleX(0.9f).scaleY(0.9f).setInterpolator(new AccelerateInterpolator()).start();
+                }
+
+                if (newState != RecyclerView.SCROLL_STATE_SETTLING){
+                    selectedMoneySource = moneySouces.get(pos);
+                    transactions.clear();
+                    transactions.addAll(modifierTransactionListByViewMode());
+                }
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
+
+
+    }
+    private ArrayList<Transaction> modifierTransactionListByViewMode() {
+        if(viewMode2.equals(ViewMode2.DAY)) return modifierTransactionListByDate();
+        if(viewMode2.equals(ViewMode2.MONTH)) return modifierTransactionListByMonth();
+        if(viewMode2.equals(ViewMode2.YEAR)) return modifierTransactionListByYear();
+        if(viewMode2.equals(ViewMode2.MANUAL)) return modifierTransactionListByManual();
+        return modifierTransactionListByDate();
+    }
+
+    private ArrayList<Transaction> modifierTransactionListByManual() {
+        ArrayList<Transaction> res = new ArrayList<>();
+
+        return res;
+    }
+
+    private ArrayList<Transaction> modifierTransactionListByYear() {
+        ArrayList<Transaction> res = new ArrayList<>();
+        return res;
+    }
+
+    private ArrayList<Transaction> modifierTransactionListByMonth() {
+        ArrayList<Transaction> res = new ArrayList<>();
+        return res;
+    }
+
+    private ArrayList<Transaction> modifierTransactionListByDate() {
+        ArrayList<Transaction> res = new ArrayList<>();
+        for(Transaction t : selectedMoneySource.getTransactionsList()) {
+            Calendar c = Calendar.getInstance();
+            c.setTimeInMillis(t.getTransactionTime().getTime());
+            int transactionDay = c.get(Calendar.DAY_OF_MONTH);
+            int transactionMonth = c.get(Calendar.MONTH);
+            int transactionYeah = c.get(Calendar.YEAR);
+            if(transactionDay == mDay && transactionMonth == mMonth && transactionYeah == mYear)
+                res.add(t);
+        }
+        return res;
+    }
+
+    private ArrayList<Double> overallRate(){
+        ArrayList<Double> res = new ArrayList<>();
+        double income, spend;
+        income = 0;
+        spend = 0;
+        for (int i = 0; i < transactions.size(); i++){
+            if (transactions.get(i).getTransactionIsIncome()){
+                income += (double) transactions.get(i).getTransactionAmount();
+            } else {
+                spend += (double) transactions.get(i).getTransactionAmount();
+            }
+        }
+        res.add(income);
+        res.add(spend);
+        return res;
+    }
+
+    private void setupDateChart(){
+        //overall Chart
+        Pie overall = AnyChart.pie();
+        ArrayList<Double> res = new ArrayList<>();
+        res = overallRate();
+        List<DataEntry> dataEntries = new ArrayList<>();
+        for (int i = 0; i < 2; i++){
+            dataEntries.add(new ValueDataEntry(overallTitle[i], res.get(i)));
+        }
+        overall.data(dataEntries);
+        this.overallChart.setChart(overall);
+    }
+
+    private void setupMonthChart(View view){
+
+    }
+
+    private void setupYearChart(View view){
+
+    }
+
+    private void setupManualChar(View view){
+
     }
 
     private String getDayOfWeek(int value) {
